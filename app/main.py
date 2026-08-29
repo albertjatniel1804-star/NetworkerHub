3c9b22c499bb760858d4a8fede541cdb5526b8b1
Albert Jatniel Matos <albertjatniel1804@gmail.com>
2026-08-28 22:25:52 -0500

    feat: add user registration and validation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from app.database.dependencies import get_db
+from app.models.user import User
+from app.security.password import hash_password
+from app.schemas.user import UserCreate
 
 app = FastAPI(title="NetworkerHub")
 
@@ -18,4 +24,60 @@ def home(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="index.html"
+
     )
+
+
+@app.get("/db-test")
+def database_test(db: Session = Depends(get_db)):
+    return {"message": "Conexión con la base de datos funcionando"}
+
+
+@app.get("/db/users/count")
+def users_count(db: Session = Depends(get_db)):
+    total_users = db.query(User).count()
+
+    return {"total_users": total_users}
+
+
+@app.post("/users")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="El correo ya está registrado"
+        )
+
+    hashed_password = hash_password(user.password)
+
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hashed_password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "id": new_user.id,
+        "name": new_user.name,
+        "email": new_user.email
+    }
+
+
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+
+    return [
+        {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email
+        }
+        for user in users
+    ]
